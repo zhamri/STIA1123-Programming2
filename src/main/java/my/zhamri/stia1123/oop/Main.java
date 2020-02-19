743e6aac41089d8f8bb637e1fcff5f60ac22d345
Zhamri Che Ani <zhamri@gmail.com>
2020-02-19 12:55:49 +0800

    Add example toString

---
+++ src/main/java/my/zhamri/stia1123/oop/Main.java
@@ -0,0 +1,17 @@
+package my.zhamri.stia1123.oop;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student zhamri = new Student("zhamri", "che ani", "sintok");
+        Student huda = new Student("nuda", "ibrahim", "jitra");
+        System.out.println(zhamri.getAddress());
+        System.out.println(huda.getLastName());
+    }
+}
+
+
+/***
+ * sintok
+ * ibrahim
+ */
