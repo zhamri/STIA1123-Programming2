41ee5b2fb2a0c1d22964bdda7563ffe4d1410ca8
Zhamri Che Ani <zhamri@gmail.com>
2020-02-19 12:36:24 +0800

    Add example toString

---
+++ src/main/java/my/zhamri/stia1123/oop/Student.java
@@ -0,0 +1,38 @@
+package my.zhamri.stia1123.oop;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String address;
+
+    public Student(String firstName, String lastName, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
