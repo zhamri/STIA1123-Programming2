ecb389203ac01ac80c9c787d5b6f7fc7c7953d36
zhamri <zhamri@gmail.com>
2023-11-06 10:34:11 +0800

    Add Car Example1

--- src/main/java/my/zhamri/stia1123/inheritance/Example1.java
+++ src/main/java/my/zhamri/stia1123/inheritance/Example1.java
@@ -39,6 +39,9 @@ Questions
 1. What is the output?
 2. How many OOP concepts have been applied in this example?
 3. Explain line 29 and 31.
+4. Divide into 4 classes. Then run the program again.
+5. Create a Bus class which is the subclass of Vehicle class.
+6. Create a ElectricCar class which is the subclass of Car class.
 
 Note: Inheritance-Car.png
  */
