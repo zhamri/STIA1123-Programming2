8b594476d9653096139a84e8e0060a069a1d26d9
zhamri <zhamri@gmail.com>
2024-04-25 13:01:44 +0800

    update

---
+++ Chapter-1_Introduction/src/main/java/my/zhamri/stia1123/App.java
@@ -0,0 +1,13 @@
+package my.zhamri.stia1123;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
